d35fbbb802fc92bbeebba68ede25bdbe2643d059
Lilla-ctrl <berenyi.lilu@gmail.com>
2023-05-26 13:07:00 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Weather from "./Weather";
 
-export default function Header() {
+export default function App() {
   return (
-    <div className="Header">
-      <h1>Weather App</h1>
-      <Weather />
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+      </div>
     </div>
   );
 }
